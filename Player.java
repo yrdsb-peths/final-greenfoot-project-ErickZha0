740520d0131d5083a1e66a836fcf27e858d1545e
ErickZha0 <341137289@gapps.yrdsb.ca>
2023-01-09 21:12:23 -0500

    movement

--- Player.java
+++ Player.java
@@ -12,14 +12,50 @@ public class Player extends Actor
      * Act - do whatever the Player wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    GreenfootImage[] idle = new GreenfootImage[10];
+    GreenfootImage[] idleRight= new GreenfootImage[9];
+    GreenfootImage[] idleLeft= new GreenfootImage[9];
+    String facing = "right";
+    SimpleTimer animationTimer = new SimpleTimer();
     
     public Player()
     {
         for(int i = 0; i < 9; i++)
         {
-            idle[i] = new GreenfootImage("images/freeknight/png/Idle0"  + i +  ".png");
+            idleRight[i] = new GreenfootImage("images/freeknight/png/Idle0"  + i +  ".png");
+            idleRight[i].scale(100, 100);
             
+        }
+        
+        for(int i = 0; i < idleLeft.length; i ++)
+        {
+            idleLeft[i] = new GreenfootImage("images/freeknight/png/Idle0" + i + ".png");    
+            idleLeft[i].mirrorHorizontally();
+            idleLeft[i].scale(100, 100);   
+        }
+        
+        animationTimer.mark();
+        
+        setImage(idleRight[0]);
+        
+    }
+    int imageIndex = 0;
+    public void animateElephant()
+    {
+        //elephant animations
+        if(animationTimer.millisElapsed() < 300)
+        {
+            return;
+        }
+        animationTimer.mark();
+        if(facing.equals("right"))
+        {
+            setImage(idleRight[imageIndex]);
+            imageIndex = (imageIndex + 1) % idleRight.length;    
+        }
+        else
+        {
+            setImage(idleLeft[imageIndex]);
+            imageIndex= (imageIndex + 1) % idleLeft.length;
         }
         
     }
@@ -29,15 +65,15 @@ public class Player extends Actor
         if(Greenfoot.isKeyDown("left"))
         {
             move(-3);
-            
+            facing = "left";
         } 
         else if(Greenfoot.isKeyDown("right"))
         {
             move(3);
-            
+            facing = "right";
             
         }
         
-        
+        animateElephant();
     }    
 }
