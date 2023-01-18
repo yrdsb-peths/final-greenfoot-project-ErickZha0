9c1d69d9675d0a9b7fe6b19c2df6edea41a57e2c
ErickZha0 <341137289@gapps.yrdsb.ca>
2023-01-17 23:07:39 -0500

    c

--- MyWorld.java
+++ MyWorld.java
@@ -22,47 +22,10 @@ public class MyWorld extends World
         Player player = new Player();
         addObject(player, 100 ,520);
 
-        prepare();
-
 
     }
-
-
-    /**
-     * Prepare the world for the start of the program.
-     * That is: create the initial objects and add them to the world.
-     */
-    private void prepare()
-    {
-        Ground1 ground1 = new Ground1();
-        addObject(ground1,61,673);
-
-        Ground1 ground1A = new Ground1();
-        addObject(ground1A,188,673);
-
-        Ground1 ground1B = new Ground1();
-        addObject(ground1B,315,673);
-
-        Ground1 ground1C = new Ground1();
-        addObject(ground1C,442,673);
-
-
-        Ground1 ground1F = new Ground1();
-        addObject(ground1F,823,673);
-
-
-
-        Crate crate = new Crate();
-        addObject(crate,467,565);
-        crate.setLocation(473,568);
-        Crate crate2 = new Crate();
-        addObject(crate2,466,482);
-        crate2.setLocation(469,495);
-        Crate crate3 = new Crate();
-        addObject(crate3,350,556);
-        crate3.setLocation(392,571);
-    }
-
 }
+
+
 
 
